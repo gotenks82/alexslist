e893d5ecd5e66df9e93a18c4dd757e4f72d2d284
Alex Pedini <alex.pedini@gmail.com>
2018-10-20 19:00:11 +0200

    * reverting a css attempt at fade in

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -5,11 +5,10 @@ function getNotifications() {
         contentType: "application/json",
         success: function (result) {
             $.each(result.notifications, function (idx, notification) {
-                $("#notifications").append("<div class=\"alert alert-success alert-dismissable fade\">\n" +
+                $("#notifications").append("<div class=\"alert alert-success alert-dismissable\">\n" +
                     " <strong>" + notification + "</strong><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">\n" +
                     "    <span aria-hidden=\"true\">&times;</span>\n" +
                     "  </button></div>");
-                $("#fade").addClass("in");
             });
         }
     });
